cb1d49378bee6aa55402224e1179747dc590138a
Sherin Olivia <sochronicle@gmail.com>
2024-02-08 21:16:30 +0700

    chore: added database details

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,11 +1,13 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: '',
-    host: 'localhost',
-    database: '',
-    password: '',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
     port: 5432,
 })
+
+console.log(pool);
 
 export default pool;
