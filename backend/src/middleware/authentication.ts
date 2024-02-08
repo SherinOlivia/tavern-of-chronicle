7383c8363dcacaf342aa7806e28b160786073e00
Sherin Olivia <sochronicle@gmail.com>
2024-02-08 21:17:35 +0700

    feat: created authentication middleware

---
+++ backend/src/middleware/authentication.ts
@@ -0,0 +1,28 @@
+import { Response, NextFunction } from "express";
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
+import JWT_TOKEN from "../config/jwt";
+import { errorHandling } from "../controller/errorHandling";
+import { CustomRequest } from "../types/custom";
+
+const authenticationMiddleware = (req: CustomRequest, res: Response, next: NextFunction) => {
+    const authentication = req.cookies.access_token;
+
+    if (!authentication) {
+        return res.status(400).json(errorHandling(null, "Unauthorized Access.."));
+    }  else {
+        try {
+            const decodedToken = jwt.verify(authentication, JWT_TOKEN as Secret) as JwtPayload;
+            console.log("Decoded Token: ", decodedToken);
+            if ('role' in decodedToken) {
+                req.user = decodedToken;
+                req.role = decodedToken.role;
+                next();
+            } else {
+                return res.status(401).json(errorHandling(null, "Unidentified Role..."));
+            }
+
+        } catch (error) {
+            return res.status(500).json(errorHandling(null, "Invalid Request..!"));
+        }
+    }
+}
