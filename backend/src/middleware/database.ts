eb3eaa2a01c431520e66b502caa728ecca785646
Sherin Olivia <sochronicle@gmail.com>
2024-02-08 21:18:57 +0700

    feat: fixed database middleware -> req type

--- backend/src/middleware/database.ts
+++ backend/src/middleware/database.ts
@@ -2,7 +2,10 @@ import { Request, Response, NextFunction } from "express";
 import { CustomRequest } from "../types/custom";
 import pool from "../config/database";
 
-export const databaseMiddleware = (req: CustomRequest, res: Response, next: NextFunction) => {
-    req.db = pool;
+export const databaseMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const customReq = req as unknown as CustomRequest;
+    customReq.db = pool;
     next(); 
 }
+
+
