2a6bcdec52650877d1f9a01306e5838468d0e83b
Sherin Olivia <sochronicle@gmail.com>
2024-02-17 23:49:50 +0700

    fix: fixed error handling controller to allow null error

--- backend/src/controller/errorHandling.ts
+++ backend/src/controller/errorHandling.ts
@@ -1,18 +1,20 @@
-export const errorHandling = function (data: any, error: string | Error) {
-    if (error instanceof Error) {
-        return {
-            success: false,
-            error: error
-        }
-    } else if (typeof error === 'string') {
-        return {
-            success: false,
-            error: new Error(error)
-        }
-    } else {
+export const errorHandling = function (data: any, error: string | Error | null) {
+    if (error === null) {
         return {
             success: true,
             data:data
+        }
+    } else {
+        if (error instanceof Error) {
+            return {
+                success: false,
+                error: error
+            }
+        } else if (typeof error === 'string') {
+            return {
+                success: false,
+                error: new Error(error)
+            }
         }
     }
 }
