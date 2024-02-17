03f98b2b62d733405dc70a215a9d36c9835ee6d9
Sherin Olivia <sochronicle@gmail.com>
2024-02-17 23:49:06 +0700

    chore: updated error handling responses

--- backend/src/controller/authentication.ts
+++ backend/src/controller/authentication.ts
@@ -10,14 +10,14 @@ const registerUser = async (req: Request, res: Response) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const result: QueryResult = await pool.query(
-            'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email',
+            'INSERT INTO users (username, email, password, role) VALUES ($1, $2, $3, "player") RETURNING id, username, email, role',
             [username, email, hashedPassword]
         );
 
-        res.status(200).json(result.rows[0]);
+        res.status(200).json(errorHandling(result.rows[0], null));
     } catch (error) {
         console.error('Error registering user:', error);
-        res.status(500).json(errorHandling(null, 'Failed to register user'));
+        res.status(500).json(errorHandling(null, 'Internal Server Error.. User Registration Request Failed.'));
     }
 };
 
@@ -38,7 +38,7 @@ const loginUser = async (req: Request, res: Response) => {
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (passwordMatch) {
-            res.status(200).json({ id: user.id, username: user.username, email: user.email });
+            res.status(200).json(errorHandling({ id: user.id, username: user.username, email: user.email },null));
         } else {
             res.status(401).json(errorHandling(null, 'Incorrect password'));
         }
