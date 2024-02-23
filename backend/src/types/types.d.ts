caa2b9020c068745884be19914028efb77eb40d8
Sherin Olivia <sochronicle@gmail.com>
2024-02-23 23:49:33 +0700

    chore: added role to user

--- backend/src/types/types.d.ts
+++ backend/src/types/types.d.ts
@@ -8,6 +8,7 @@ declare global {
     namespace Express {
         interface Request {
             user?: {
+                id: string;
                 name: string;
                 role: string;
             };
