f7444de07566cb9ba6d4ceae86cbdd4ed56fb1fd
Sherin Olivia <sochronicle@gmail.com>
2024-02-25 00:01:42 +0700

    feat: created main router

---
+++ backend/src/router/mainRouter.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import authenMiddleware from '../middleware/authenticationMiddleware';
+import { authRouter } from '../router';
+
+const router = express.Router();
+
+router.get("/", function (req: Request, res: Response) {
+    res.status(200).json({
+        success: true,
+        message: "Hello, Welcome to Tavern of Chronicle! Come on in!"
+    })
+})
+
+router.use('/api/v1/auth', authRouter)
+// router.use('/api/v1/user', authenMiddleware, userRouter)
+
+
+export default router;
