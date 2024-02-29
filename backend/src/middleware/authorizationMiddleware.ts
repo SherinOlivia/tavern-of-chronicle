f2f3b0f08a854fb87ea2352803ba83d35be7cd66
Sherin Olivia <sochronicle@gmail.com>
2024-02-29 23:33:53 +0700

    chore: changed CustomResponse to Response

--- backend/src/middleware/authorizationMiddleware.ts
+++ backend/src/middleware/authorizationMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomResponse } from '../types/types';
 import { errorHandling } from '../controller/errorHandling';
 
-const authorizationMiddleware = (roles: string[]) => (req: Request, res: CustomResponse, next: NextFunction) => {
+const authorizationMiddleware = (roles: string[]) => (req: Request, res: Response, next: NextFunction) => {
     if (req.role && !roles.includes(req.role)) {
         return res.status(401).json(errorHandling(null, "Unauthorized Access~"));
     } 
