760d563252accb569599b29062ea1ef7dc9173a5
Sherin Olivia <sochronicle@gmail.com>
2024-02-29 23:30:29 +0700

    chore: removed gamemaster.ts, moved get user list to user.ts

--- backend/src/controller/user.ts
+++ backend/src/controller/user.ts
@@ -21,4 +21,28 @@ const viewUserProfile = async (req: Request, res: Response) => {
     }
 }
 
-export { viewUserProfile }
+const getUserList = async (req: Request, res: Response) => {
+    const user = req.user
+    try {
+        
+        if (!user) {
+            res.status(400).json(errorHandling(null,"Please login..!"));
+        } else {
+            const checkAdminRole = await pool.query("SELECT * from users WHERE role = $1",[user.role])
+            if (!checkAdminRole) {
+                res.status(400).json(errorHandling(null,"Unauthorized Access..!!!!!"));
+            } else {
+                const userList = await pool.query("SELECT * from users")
+                return res.status(200).json({
+                    success: true,
+                    message: `List of User Profiles`,
+                    data: userList 
+                });
+            }
+        }
+    } catch (error) {
+        return res.status(500).json(errorHandling(null, "Internal Error.. Failed to view User's Profile"));
+    }
+}
+
+export { viewUserProfile, getUserList }
