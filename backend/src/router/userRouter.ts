742a28b9bca68dae4cfbf00e193fc62214c04fe9
Sherin Olivia <sochronicle@gmail.com>
2024-02-29 23:34:15 +0700

    feat: added route to get user list

--- backend/src/router/userRouter.ts
+++ backend/src/router/userRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { viewUserProfile } from '../controller/user';
+import { getUserList, viewUserProfile } from '../controller/user';
+import authorizationMiddleware from '../middleware/authorizationMiddleware';
 
 const userRouter = express.Router()
 
 userRouter.get('/profile', viewUserProfile);
+userRouter.get('/list', authorizationMiddleware(['game_master']), getUserList);
 
 export default userRouter;
